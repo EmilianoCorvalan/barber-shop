4ed743b4de8317961e681a8540da021d666671f0
Emiliano Corvalan <emiliano.corvalan@gmail.com>
2023-12-06 14:02:17 -0300

    md table informe combinado

--- informes/infoCompleto.php
+++ informes/infoCompleto.php
@@ -16,25 +16,24 @@ $fInicial;
 $fFinal;
 $auxFecha;
 
-if ($barbero != 'none'){
+if ($barbero != 'none') {
     //print ('Entro a barbero');
-    $stringBarbero = " turnos.idbarbero = '".$barbero."' ";
+    $stringBarbero = " turnos.idbarbero = '" . $barbero . "' ";
 }
-if ($servicio != 'none'){
+if ($servicio != 'none') {
     //print ('Entro a servicio');
-    $stringServicio = " turnos.idservicio = '".$servicio."' ";
+    $stringServicio = " turnos.idservicio = '" . $servicio . "' ";
 }
 //Si no ponemos fecha ni periodo asignamos para que tome los del dia
-if ( empty($fecha1) and empty($fecha2)  ){
+if (empty($fecha1) and empty($fecha2)) {
     $fecha1 = date('Y-m-d');
     $cuanto = "dia";
-
 }
-if ( !empty($fecha1) and $cuanto == 'none'){
+if (!empty($fecha1) and $cuanto == 'none') {
     $cuanto = "dia";
 }
 
-if ( !empty($fecha1) and !empty($fecha2) ){ //SI TENEMOS LAS DOS FECHAS ENTONCES ESTO.
+if (!empty($fecha1) and !empty($fecha2)) { //SI TENEMOS LAS DOS FECHAS ENTONCES ESTO.
     if ($fecha1 >= $fecha2) {
         $fInicial = $fecha2;
         $fFinal = $fecha1;
@@ -42,38 +41,38 @@ if ( !empty($fecha1) and !empty($fecha2) ){ //SI TENEMOS LAS DOS FECHAS ENTONCES
         $fInicial = $fecha1;
         $fFinal = $fecha2;
     }
-}else if (!empty($fecha1) and $cuanto != 'none') { //SI TENEMOS FECHA INICIAL Y CUANTO ENTONCES ESTO.
-        $fInicial = $fecha1;
-        $auxFecha = new DateTime($fecha1);
-        if ($cuanto == "dia") {
-            $intervalo = new DateInterval('P1D');
-            $auxFecha->add($intervalo);
-        } else if ($cuanto == "semana") {
-            $intervalo = new DateInterval('P7D');
-            $auxFecha->add($intervalo);
-        } else if ($cuanto == "mes") {
-            $intervalo = new DateInterval('P1M');
-            $auxFecha->add($intervalo);
-        } else if ($cuanto == "anio") {
-            $intervalo = new DateInterval('P12M');
-            $auxFecha->add($intervalo);
-        }
-        $fFinal = $auxFecha->format('Y-m-d'); 
+} else if (!empty($fecha1) and $cuanto != 'none') { //SI TENEMOS FECHA INICIAL Y CUANTO ENTONCES ESTO.
+    $fInicial = $fecha1;
+    $auxFecha = new DateTime($fecha1);
+    if ($cuanto == "dia") {
+        $intervalo = new DateInterval('P1D');
+        $auxFecha->add($intervalo);
+    } else if ($cuanto == "semana") {
+        $intervalo = new DateInterval('P7D');
+        $auxFecha->add($intervalo);
+    } else if ($cuanto == "mes") {
+        $intervalo = new DateInterval('P1M');
+        $auxFecha->add($intervalo);
+    } else if ($cuanto == "anio") {
+        $intervalo = new DateInterval('P12M');
+        $auxFecha->add($intervalo);
+    }
+    $fFinal = $auxFecha->format('Y-m-d');
 }
 
 $filtro = ' WHERE ';
-if ($stringBarbero != ""){//.' WHERE '
-    $filtro = $filtro.$stringBarbero;
+if ($stringBarbero != "") { //.' WHERE '
+    $filtro = $filtro . $stringBarbero;
 }
-if ($stringBarbero =="" and $stringServicio != ""){ //. ' WHERE '
-    $filtro = $filtro.$stringServicio;
+if ($stringBarbero == "" and $stringServicio != "") { //. ' WHERE '
+    $filtro = $filtro . $stringServicio;
 } else if ($stringServicio != "") {
-    $filtro = $filtro.' AND '.$stringServicio;
+    $filtro = $filtro . ' AND ' . $stringServicio;
 }
-if ($stringBarbero!="" or $stringServicio !=""){
-    $filtro = $filtro." AND ";
+if ($stringBarbero != "" or $stringServicio != "") {
+    $filtro = $filtro . " AND ";
 }
-$filtro = $filtro. "turnos.fecha >= ".$fInicial." AND turnos.fecha <= ".$fFinal;
+$filtro = $filtro . "turnos.fecha >= '" . $fInicial . "' AND turnos.fecha <= '" . $fFinal . "'";
 
 
 
@@ -94,22 +93,25 @@ $query2 = $conn->query($total_query);
 
 
 ?>
-<div class="bloque">Informe de Facturación </div>
-<div>
-    <table>
-        <thead>
-            <td scope="col">Fecha</td>
-            <td scope="col">Hora</td>
-            <td scope="col">Email</td>
-            <td scope="col">Nombre</td>
-            <td scope="col">Apellido</td>
-            <td scope="col">Servicio</td>
-            <td scope="col">Valor</td>
-        </thead>
-        <tbody>
-            <?php
-            while ($fila = mysqli_fetch_array($query)) {
-                echo "<tr class='fila'>
+<div class="container-fluid">
+    <div class="container">
+        <h2 class="text-white text-center p-2">Informe de facturación</h2>
+        <table class="table table-dark table-hover">
+            <thead>
+                <tr>
+                    <td scope="col">Fecha</td>
+                    <td scope="col">Hora</td>
+                    <td scope="col">Email</td>
+                    <td scope="col">Nombre</td>
+                    <td scope="col">Apellido</td>
+                    <td scope="col">Servicio</td>
+                    <td scope="col">Valor</td>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                while ($fila = mysqli_fetch_array($query)) {
+                    echo "<tr class='fila'>
                         <td>" . $fila['fecha'] . "</td>
                         <td>" . $fila['time'] . "</td>
                         <td>" . $fila['mail'] . "</td>
@@ -117,11 +119,11 @@ $query2 = $conn->query($total_query);
                         <td>" . $fila['apellido'] . "</td>
                         <td>" . $fila['tiposervicio'] . "</td>
                         <td class='numeracion'>" . $fila['valor'] . "</td>";
-            }
-            ?><br>
-            <?php
-            $total = mysqli_fetch_row($query2);
-            echo "<tr>
+                }
+                ?><br>
+                <?php
+                $total = mysqli_fetch_row($query2);
+                echo "<tr>
                         <td></td>
                         <td></td>
                         <td></td>
@@ -130,12 +132,12 @@ $query2 = $conn->query($total_query);
                         <td></td>
                         <td class='numeracion total'>" . $total[0] . "</td>
                     </tr>";
-            ?>
+                ?>
 
-        </tbody>
-    </table>
+            </tbody>
+        </table>
 
-    <button id="printButton" style="padding: 10px 15px;
+        <button id="printButton" style="padding: 10px 15px;
                                     font-size: 14px;
                                     background-color: #fff;
                                     color: #000;
@@ -143,12 +145,10 @@ $query2 = $conn->query($total_query);
                                     cursor: pointer;
                                     margin: 2rem;
                                     display: block;
-                                    margin: 15px auto;" 
-                                    onmouseover="this.style.backgroundColor='#000'; this.style.color='#fff';"
-                                    onmouseout="this.style.backgroundColor='#fff'; this.style.color='#000';">
-        Imprimir</button>
+                                    margin: 15px auto;" onmouseover="this.style.backgroundColor='#000'; this.style.color='#fff';" onmouseout="this.style.backgroundColor='#fff'; this.style.color='#000';">
+            Imprimir</button>
+    </div>
 </div>
-
 <script>
     document.getElementById('printButton').addEventListener('click', function() {
         window.print();
